3ab8d3da18f903d093fb008b5d4f776eddbac539
Trubenko-Valeria <trubenko.valeria@gmail.com>
2023-03-30 23:24:25 +0300

    Attached a API link to the project

--- src/App.jsx
+++ src/App.jsx
@@ -6,30 +6,32 @@ import Header from "./components/Header/Header";
 import Conversion from "./components/Conversion/Conversion";
 
 class App extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.getRate();
-  // }
-  // getRate = () => {
-  //   fetch("https://bank.gov.ua/NBU_Exchange/exchange?json", {
-  //     mode: "no-cors",
-  //   })
-      // .then((data) => {
-      //   return data.json;
-      // })
-  //     .then((response) => {
-  //       console.log(response);
-  //     });
-  // };
-
-  render() {
-    return (
-      <div className='App'>
-        <Header />
-        <Conversion />
-      </div>
-    );
+  constructor(props) {
+    super(props);
+    this.getRate();
   }
-}
+
+  getRate = () => { 
+
+    fetch(
+      "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/uah.json"
+    )
+      .then((data) => {
+        return data.json();
+      })
+      .then((data) => {
+        console.log("DATA", data);
+      });
+  }
+
+    render() {
+      return (
+        <div className="App">
+          <Header />
+          <Conversion />
+        </div>
+      );
+    }
+  }
 
 export default App;
--- src/components/Conversion/Conversion.jsx
+++ src/components/Conversion/Conversion.jsx
@@ -1,8 +1,10 @@
 // import logo from './logo.png';
 import { Component } from "react";
+import {useState} from 'react';
 import "../css/Conversion.css";
 
 class Conversion extends Component {
+
   render() {
     return (
       <div className="head">
@@ -10,38 +12,50 @@ class Conversion extends Component {
         <h1 className="head__title">Currency conversion</h1>
         <div className="head__block">
           <div className="head__block__wrapper">
-            <label className="head__block__wrapper__label" for="numb_1">
+            <label className="head__block__wrapper__label" htmlFor="numb_1">
               Exchange
             </label>
             <input
               className="head__block__wrapper__input"
               type="number"
               id="numb_1"
+              placeholder="Enter the amount"
+              // onChange={(e) => setInput(e.target.value)}
             ></input>
-            <select className="head__block__wrapper__select">
-              <option value="value1" selected>
+            {/* <select
+              className="head__block__wrapper__select"
+              value={this.state.value}
+              onChange={this.handleChange}
+            >
+              <option defaultValue="value1" selected>
                 USD
               </option>
-              <option value="value2">UAH</option>
-              <option value="value3">EUR</option>
-            </select>
+              <option defaultValue="value2">UAH</option>
+              <option defaultValue="value3">EUR</option>
+            </select> */}
           </div>
           <div className="head__block__wrapper">
-            <label className="head__block__wrapper__label" for="numb_2">
+            <label className="head__block__wrapper__label" htmlFor="numb_2">
               Receive
             </label>
             <input
               className="head__block__wrapper__input"
               type="number"
               id="numb_2"
+              placeholder="Enter the amount"
+              // onChange={(e) => setInput(e.target.value)}
             ></input>
-            <select className="head__block__wrapper__select">
-              <option value="value1" selected>
+            {/* <select
+              className="head__block__wrapper__select"
+              value={this.state.value}
+              onChange={this.handleChange}
+            >
+              <option defaultValue="value1" selected>
                 USD
               </option>
-              <option value="value2">UAH</option>
-              <option value="value3">EUR</option>
-            </select>
+              <option defaultValue="value2">UAH</option>
+              <option defaultValue="value3">EUR</option>
+            </select> */}
           </div>
         </div>
       </div>
